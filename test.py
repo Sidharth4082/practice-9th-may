26162e41a778056186755abb09833011826ca701
Sidharth <sidharthkumar3000@gmail.com>
2023-05-23 21:20:38 +0530

    added new

--- test.py
+++
@@ -1,7 +0,0 @@
-import BeautifulSoup
-import requests
-
-response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
-webpage = response.text
-soup = BeautifulSoup(webpage, "html.parser")
-print(soup.find_all(name="h3", class_="jsx-4245974604"))
